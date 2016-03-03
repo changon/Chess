33b881bb3d20e9ab33aae595dc727fdd24b9fecc
Raphael Kim <raphael.c.kim@gmail.com>
2016-03-03 11:47:23 -0500

    test mod

--- Play.java
+++ Play.java
@@ -8,8 +8,8 @@ public class Play
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Time to play Chess! One player or Two player? (1, 2)");
 		int value = Integer.parseInt(scan.next());
-		if (value ==1) {board = new Board(1); }
-		else if (value ==2 ) { board = new Board(2); }
-		else { System.out.println("type a valid option, 1 or 2");}		
+		if (value ==1) board = new Board(1);
+		else if (value ==2 ) board = new Board(2); 
+		else {System.out.println("type a valid option, 1 or 2");}	
 	}
 }
